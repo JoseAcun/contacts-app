8adca7923e01ecf3e688780a0a09a1a74e844f04
Jose Acuña <josesanti09@hotmail.com>
2022-05-10 16:13:00 -0500

    create users

--- partials/header.php
+++ partials/header.php
@@ -16,7 +16,7 @@
     <!-- Static Content -->
     <link rel="stylesheet" href="./static/css/index.css" />
     <?php $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH) ?>
-    <?php if ($uri == "/contacts-app/index.php"): ?>
+    <?php if ($uri == "/contacts-app/index.php" || $uri == "/contacts-app/index.php"): ?>
     <script defer src="./static/js/welcome.js"></script>
     <?php endif ?>
     <title>Contacts App</title>
--- sql/setup.sql
+++ sql/setup.sql
@@ -9,5 +9,13 @@ create table contacts (
     name varchar(255)
     phone_number varchar(255)
 );
+create Table users (
+    id int auto_increment PRIMARY KEY,
+    name varchar (255),
+    email varchar (255),
+    password varchar(255)
+);
 
 insert into contacts (name, phone_number) values ("pepe", "3123214124");
+
+
